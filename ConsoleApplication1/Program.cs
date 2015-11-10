ac7cabe8f0d07f7ee77ab4f639951e888145db2c
YEKU1231 <karabina.x@gmail.com>
2015-11-11 00:09:01 +0900

    tes

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,11 +11,10 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
-            //@@@@
+            //test!!!
 
 
 
         }
-        static void Test() { }
     }
 }
